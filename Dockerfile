7a6b9877bf4b4cb0c663ae7026c78e079eb8ea79
ibis-miguel <miguel.lachman@ibis-management.com>
2025-04-03 07:42:25 -0400

    env added

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:21-jdk-slim
 WORKDIR /app
 COPY target/quickquid-0.0.1-SNAPSHOT.jar /app/app.jar
 EXPOSE 8080
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app.jar", "--spring.profiles.active=prod"]
--- src/main/java/com/ibis/quickquid/configs/WebConfig.java
+++ src/main/java/com/ibis/quickquid/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://64.225.62.232:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
